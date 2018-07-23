811ab4da802ef864660353dd8187153b4b848aea
wackyjoker <kevin.young8931@gmail.com>
2018-07-23 14:11:06 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
+
+const jsx = (
+    <div>
+    <h1>welcome</h1>
+    </div>
+);
+
+ReactDOM.render(jsx,document.getElementById('app'));
