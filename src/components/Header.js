ce6b97b3bcab6e3f7d36857417731b99d331b1b2
wackyjoker <kevin.young8931@gmail.com>
2018-11-05 15:07:34 +0100

    5 of NOV update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import React from 'react';
 const Header = ()=>(
     <header id='header'>
     <nav className="navbar">
-    <div className="">
+    
     <a className="" href="#">WebSiteName</a>
-      </div>
+    
     <div id="pageList">
     <ul id="header-ul">
       <li id='header-li'><a href="#">Home</a></li>
--- src/components/Section1.js
+++ src/components/Section1.js
@@ -3,10 +3,9 @@ import React from 'react';
 
 const Section1 = ()=>(
 <section  className="section1">
-<canvas id='bg1'></canvas>
 <div id="inner">
 <h2 id='s1h2'>WAC<span>K<span>evin</span></span><span>Y<span>oung</span></span></h2>
-<p>bacon ipsum etcs about these things</p>
+<p id='content'>bacon ipsum etcs about these things</p>
 </div>
 <a id='intro' href="#section2" >More About Me</a>
 </section>
