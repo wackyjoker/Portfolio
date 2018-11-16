09ccf08df1bd29a1d19a60c6e1d19cd4779e0222
wackyjoker <kevin.young8931@gmail.com>
2018-11-16 02:51:13 +0100

    night work

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -5,7 +5,10 @@ const Section1 = ()=>(
 <section  className="section1">
 <div id="inner">
 <h2 id='s1h2'>WAC<span>K<span>evin</span></span><span>Y<span>oung</span></span></h2>
-<p id='content'>bacon ipsum etcs about these things</p>
+<div id='content'> 
+  <h2>Hello Hola 你好</h2> 
+  <p>i grind fullstack and responsive web design</p>
+</div>
 </div>
 <a id='intro' href="#section2" >More About Me</a>
 </section>
