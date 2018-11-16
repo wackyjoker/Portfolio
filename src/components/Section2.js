6c32dfd2a93cb3bd8a41cac82aa618e4a68bbc70
wackyjoker <kevin.young8931@gmail.com>
2018-11-16 20:35:42 +0100

    today's work

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const Section2 =()=>(
-    <div>
-    <section id="section2">
+    
+    <div id='moda'>
+    {/*<section id="section2">
     <header id='major' >
     <h2 >this is h2</h2>
     </header>
-    </section>
+</section>*/}
     </div>
+    
 );
 
 export default Section2;
