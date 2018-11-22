b1771c2edaa7a40a111fa634cd9925227b5485db
wackyjoker <kevin.young8931@gmail.com>
2018-11-22 22:05:56 +0100

    barfix

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Header = ()=>(
     <header id='header'>
+    {/* small device bar */}
+    <div id='burger'>
+     <div></div>
+     <div></div>
+     <div></div>
+    </div>
     <nav className="s1-navbar">
     
+
     
     <a id='head-anc' href='#'>WAC<span>K<span>evin</span></span><span>Y<span>oung</span></span></a>
     <div id="pageList">
@@ -14,6 +21,7 @@ const Header = ()=>(
       <li id='header-li'><a href="#">Page 3</a></li>
     </ul>
     </div>
+    
     </nav>
     </header>
 );
