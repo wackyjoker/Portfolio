2e99e041661677188ad8c1c63a118c13d9f3a621
wackyjoker <kevin.young8931@gmail.com>
2018-11-26 20:13:11 +0100

    。。。

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,24 @@ import React from 'react';
 const Header = ()=>(
     <header id='header'>
     {/* small device bar */}
-    <div id='burger'>
+    <div id='burger' >
+    <label htmlFor='show-menu'>
      <div></div>
      <div></div>
      <div></div>
+     </label>
     </div>
-    <nav className="s1-navbar">
-    
+    <label htmlFor='show-menu' >Menu</label>
+    <input type='checkbox' id='show-menu' role='button' />
+    <ul id="header-ul-mobile">
+      <li id='header-li'><a href="#">Home</a></li>
+      <li id='header-li'><a href="#">Page 1</a></li>
+      <li id='header-li'><a href="#">Page 2</a></li>
+      <li id='header-li'><a href="#">Page 3</a></li>
+    </ul>
 
-    
+    {/* normal header*/}
+    <nav className="s1-navbar">
     <a id='head-anc' href='#'>WAC<span>K<span>evin</span></span><span>Y<span>oung</span></span></a>
     <div id="pageList">
     <ul id="header-ul">
