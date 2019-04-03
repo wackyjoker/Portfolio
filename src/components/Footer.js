fb0acad02895ebc185c617fe65596ea971ab5539
wackyjoker <kevin.young8931@gmail.com>
2019-04-03 20:22:15 +0200

    setting footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const Footer = ()=>(
+
+<footer>
+<div id='footer-container'>
+
+
+</div>
+<div id='rights'>
+<p>©copyrights 2018 wacKYJoker All rights Served.</p>
+</div>
+</footer>
+
+)
+
+
+export default Footer;
--- src/routers/AppRouters.js
+++ src/routers/AppRouters.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route,Switch,Link,NavLink} from 'react-router-dom';
 import Header from '../components/Header';
 import Section2 from '../components/Section2';
 import Section1 from '../components/Section1';
+import Footer from '../components/Footer';
 
 const AppRouter=()=>(
     <BrowserRouter>
@@ -12,6 +13,7 @@ const AppRouter=()=>(
     <Switch>
     <Route path="/" component={Section2} exact={true}/>
     </Switch>
+    <Footer />
     </div>
     </BrowserRouter>
 );
