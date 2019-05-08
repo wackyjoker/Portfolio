d4c004cfba63fd1ed5af1a1755a499e1530ff257
wackyjoker <kevin.young8931@gmail.com>
2019-05-08 13:17:25 +0200

    section 2 fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,17 @@ const Footer = ()=>(
 <footer>
 <div id='footer-container'>
 
+<div id='ftRights'>
+<p>©copyrights 2018 wacKYJoker All rights Served.</p>
+<div className='icons'>
+
+<a href='#'><svg className="icon facebook"/></a>
+<a href='#'><svg className='icon github'/></a>
+<a href='#'><svg className='icon twitter'/></a>
+<a href='#'><svg className='icon linkedin'/></a>
 
 </div>
-<div id='rights'>
-<p>©copyrights 2018 wacKYJoker All rights Served.</p>
+</div>
 </div>
 </footer>
 
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -4,8 +4,34 @@ const Section2 =()=>(
     
     
     <section id="section2">
+    <div className="s2-container">
+    <h2 >ABOUT</h2>
+    <hr />
+         <div className='svg-wrap'>
+
+           <div className='box1 content-box'> <svg className="icon fast" />
+           Fast
+           <p>Fast load times and lag free interaction, my highest priority.</p>
+           </div>
+
+           <div className='box2 content-box'> 
+            <svg className="icon responsive" />
+            Responsive
+            <p>
+            My layouts will work on any device, big or small. </p>
+            </div>
+
+            
+            <div className='box3 content-box'>
+            <svg className="icon dynamic" />
+            Dynamic
+            <p>Websites don't have to be static, I love making pages come to life.
+            </p></div>
+            <svg className="icon" />
+            
+         </div>
+    </div>
     
-    <h2 >this is h2</h2>
     
 </section>
     
