6940160c9a00070640f651ddb552e84831bbe80e
wackyjoker <kevin.young8931@gmail.com>
2019-05-11 16:43:19 +0200

    test stage 2

--- server/server.js
+++ server/server.js
@@ -17,13 +17,14 @@ app.use("/images", express.static(__dirname + "../public/images"));
 app.use(favicon(path.join(__dirname, "../public/images", "eric.ico")));
 
 //server static if production
-if (process.env.NODE_ENV === "production") {
-  //set static folder
-  app.use(express.static(publicPath));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
-  });
-}
+
+//set static folder
+
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
 app.listen(process.env.PORT || 3000, err => {
   if (err) {
     console.log(err);
